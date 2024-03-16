3b986772ae74590165f09196214cc4c868dc24f9
DarioGB1 <darioguzmanbz@gmail.com>
2024-03-16 16:08:30 -0400

    implementacion del slider de productos

--- src/presentation/views/Home.jsx
+++ src/presentation/views/Home.jsx
@@ -1,10 +1,12 @@
 import {Outlet} from "react-router-dom";
 import './home.css'
 import Carousel from "../components/Carousel.jsx";
-export default function Home() {
+import FlashCard from "../components/ofertas/FlashCard.jsx";
+import FlashDeals from "../components/ofertas/FlashDeals.jsx";
+export default function Home({productItems}) {
 
-     return <div className='container'>
-         <Carousel/>
-
-     </div>
+    return <div className='container'>
+        <Carousel/>
+        <FlashDeals productItems={productItems}/>
+    </div>
 }

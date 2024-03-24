62f5ca4e9ce228cd054f3c1fdffe69d6e6d8ef7c
Jose Alem Rodriguez Valverde <josealem03@gmail.com>
2024-03-24 01:16:36 -0400

    Cambios

--- src/presentation/views/Login.jsx
+++ src/presentation/views/Login.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import './Login.css';
 import { Link, useNavigate } from 'react-router-dom';
-import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'firebase/auth';
+import {
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    FacebookAuthProvider
+} from 'firebase/auth';
 import { doc, setDoc, getDoc } from 'firebase/firestore';
-import { auth, db } from '../../infraestructure/firebase--config';
+import { auth, db } from '../../infraestructure/firebase--config'; // Asegúrate de que esta ruta sea correcta
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 export default function Login() {
     const [email, setEmail] = useState('');
@@ -14,23 +18,37 @@ export default function Login() {
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-        setError('');
+    const handleEmailChange = (event) => setEmail(event.target.value);
+    const handlePasswordChange = (event) => setPassword(event.target.value);
+
+    const handleError = (error) => {
+        console.error("Error de inicio de sesión:", error.code);
+        setError(`Error al iniciar sesión. ${error.message}`);
     };
 
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-        setError('');
+    const saveUserData = async (user) => {
+        const userRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(userRef);
+
+        if (!docSnap.exists()) {
+            await setDoc(userRef, {
+                email: user.email,
+                names: user.displayName || '',
+                birtday: '',
+                gender: '',
+                // Agrega aquí más campos según necesites, por ejemplo:
+                avatar: user.photoURL || '',
+            });
+        }
     };
 
-    const loginUser = async (email, password) => {
+    const loginUser = async (event) => {
+        event.preventDefault();
         try {
             await signInWithEmailAndPassword(auth, email, password);
             navigate('/');
         } catch (error) {
-            console.error("Error de inicio de sesión:", error.code);
-            setError('Error al iniciar sesión. Por favor, revisa tus credenciales e intenta de nuevo.');
+            handleError(error);
         }
     };
 
@@ -38,60 +56,28 @@ export default function Login() {
         try {
             const provider = new GoogleAuthProvider();
             const result = await signInWithPopup(auth, provider);
-            const user = result.user;
-
-            // Verificar si el usuario ya existe en Firestore antes de intentar crear uno nuevo
-            const userRef = doc(db, "users", user.uid);
-            const docSnap = await getDoc(userRef);
-
-            if (!docSnap.exists()) {
-                // Aquí puedes ajustar según los datos que desees almacenar
-                await setDoc(doc(db, "users", user.uid), {
-                    email: user.email,
-                    names: user.displayName,
-                    // Añade aquí más campos según los datos disponibles y tus necesidades
-                });
-            }
-
-            navigate('/'); // Redirigir al usuario después del inicio de sesión
+            await saveUserData(result.user);
+            navigate('/');
         } catch (error) {
-            console.error("Error al iniciar sesión con Google:", error);
-            setError('Error al iniciar sesión con Google. Por favor, intenta de nuevo.');
+            handleError(error);
         }
     };
 
     const signInWithFacebook = async () => {
-        const provider = new FacebookAuthProvider();
         try {
+            const provider = new FacebookAuthProvider();
             const result = await signInWithPopup(auth, provider);
-            const user = result.user;
-            const userRef = doc(db, "users", user.uid);
-            const docSnap = await getDoc(userRef);
-
-            if (!docSnap.exists()) {
-                // Crear un documento para el usuario con la información obtenida
-                await setDoc(doc(db, "users", user.uid), {
-                    email: user.email,
-                    // Añadir más campos como se requiera
-                });
-            }
-
+            await saveUserData(result.user);
             navigate('/');
         } catch (error) {
-            console.error("Error al iniciar sesión con Facebook: ", error);
-            setError('Error al iniciar sesión con Facebook. Por favor, intenta de nuevo.');
+            handleError(error);
         }
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        loginUser(email, password);
     };
 
     return (
         <section className="login-container">
             <div className="login-box">
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={loginUser}>
                     <h2>Iniciar sesión</h2>
                     {error && <div className="error-message">{error}</div>}
                     <div className={`input-box ${email ? 'active' : ''}`}>
@@ -118,16 +104,14 @@ export default function Login() {
                         />
                         <label>Contraseña</label>
                     </div>
-                    <button type="submit">Iniciar sesión</button>
-                    {/* Botón de inicio de sesión con Google */}
+                    <button type="submit" className="login-btn">Iniciar sesión</button>
                     <button type="button" onClick={signInWithGoogle} className="google-btn">
                         <FontAwesomeIcon icon={faGoogle} className="google-icon" />
-                        <span className="google-btn-text">Google</span>
+                        <span className="btn-text">Google</span>
                     </button>
-
-                    <button className="facebook-btn" onClick={signInWithFacebook} >
-                        <FontAwesomeIcon icon={faFacebookF} className="facebook-icon" />
-                        <span className="facebook-btn-text">Facebook</span> 
+                    <button type="button" onClick={signInWithFacebook} className="facebook-btn">
+                        <FontAwesomeIcon icon={faFacebookF} className="icon" />
+                        <span className="btn-text">Facebook</span>
                     </button>
                     <div className="register-link">
                         <p>¿Aún no tienes una cuenta? <Link to="/registrarse">Registrarse</Link></p>

788e2c674d618d0c5664e72dedceafe404c541ff
Jose Alem Rodriguez Valverde <josealem03@gmail.com>
2024-03-25 15:51:28 -0400

    NOTOCAR hecho con el hasheo de los usuarios en su pantallada de perfil

--- src/presentation/views/user/UserProfile.jsx
+++ src/presentation/views/user/UserProfile.jsx
@@ -1,61 +1,103 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { auth, db } from '../../../infraestructure/firebase--config'; // Asegúrate de que este archivo contiene tus exportaciones de Firebase
+import { doc, getDoc } from "firebase/firestore";
 
-export default function UserProfile(){
-    return <div className='user-profile-container'>
-        <div className="user-cont">
-            <div className="user-image-cont">
-                <div className="user-image-div">
-                    <img src='src/presentation/assets/user-profile.png' alt='perfil' className='user-img'/>
-                </div>
-                <div className="below-info">
-                    <span className='user-info-name'>Adrian Herrero</span>
-                    <span className='user-info-email'>adrian@gmail.com </span>
-                     </div>
-                {/*<div className="btn-profile">
-                    <button className='img-edit'> Editar foto de perfil</button>
-                </div>*/}
-            </div>
+class AdminInfo extends Component {
+    state = {
+        userData: null,
+    };
 
-            <div className="user-info">
-                <div className="user-info-div">
-                    <div className="stat">
-                        <h4>Nombre: </h4>
-                        <span className='user-info-span'>Adrian</span>
+    componentDidMount() {
+        this.loadUserData();
+    }
+
+    loadUserData = async () => {
+        const user = auth.currentUser;
+        if (user) {
+            const userRef = doc(db, 'users', user.uid);
+            try {
+                const docSnap = await getDoc(userRef);
+                if (docSnap.exists()) {
+                    this.setState({ userData: docSnap.data() });
+                } else {
+                    console.log('No se encontró el documento!');
+                }
+            } catch (error) {
+                console.log('Error al obtener el documento:', error);
+            }
+        } else {
+            // El usuario no está autenticado
+            console.log('El usuario no está autenticado.');
+        }
+    };
+
+    render() {
+        const { userData } = this.state;
+
+        if (!userData) {
+            return <div>Cargando...</div>;
+        }
+
+        // Desestructuramos los datos para facilitar su uso en el JSX
+        const { avatar, names, email, gender, address, birthday_date, ci, userTypeID } = userData;
+
+        return (
+            <div className='user-profile-container'>
+                <div className="user-cont">
+                    <div className="user-image-cont">
+                        <div className="user-image-div">
+                            <img src={avatar || '/src/presentation/assets/user-profile.png'} alt='perfil' className='user-img' />
+                        </div>
+                        <div className="below-info">
+                            <span className='user-info-name'>{names || 'Nombre no disponible'}</span>
+                            <span className='user-info-email'>{email || 'Email no disponible'}</span>
+                        </div>
                     </div>
-                    <div className="stat">
-                        <h4>Apellido: </h4>
-                        <span>Herrero </span>
-                    </div>
-                    <div className="stat">
-                        <h4>Fecha de Nacimiento: </h4>
-                        <span>25/11/99</span>
-                    </div>
-                </div>
-                <div className="user-info-div">
-                    <div className="stat">
-                        <h4>Dirección: </h4>
-                        <span>Av Piray 6to anillo</span>
-                    </div>
-                    <div className="stat">
-                        <h4>Correo Electronico: </h4>
-                        <span>adrian@gmail.com</span>
-                    </div>
-                    <div className="stat">
-                        <h4>Genero: </h4>
-                        <span>Masculino</span>
-                    </div>
-                </div>
-                <div className="user-info-div">
-                    <div className="stat">
-                        <h4>NIT: </h4>
-                        <span></span>
-                    </div>
-                    <div className="stat">
-                        <h4>Nro Telefono:</h4>
-                        <span>70205020</span>
+
+                    <div className="user-info">
+                        <div className="user-info-div">
+                            <div className="stat">
+                                <h4>Nombre: </h4>
+                                <span className='user-info-span'>{names?.split(" ")[0] || 'Nombre'}</span>
+                            </div>
+                            <div className="stat">
+                                <h4>Apellido: </h4>
+                                <span>{names?.split(" ").slice(1).join(" ") || 'Apellido'}</span>
+                            </div>
+                            <div className="stat">
+                                <h4>Fecha de Nacimiento: </h4>
+                                <span>{birthday_date || ''}</span>
+                            </div>
+                        </div>
+                        <div className="user-info-div">
+                            <div className="stat">
+                                <h4>Dirección: </h4>
+                                <span>{address || ''}</span>
+                            </div>
+                            <div className="stat">
+                                <h4>Correo Electrónico: </h4>
+                                <span>{email || ''}</span>
+                            </div>
+                            <div className="stat">
+                                <h4>Género: </h4>
+                                <span>{gender || ''}</span>
+                            </div>
+                        </div>
+                        <div className="user-info-div">
+                            <div className="stat">
+                                <h4>Carnet de Identidad: </h4>
+                                <span>{ci || ''}</span>
+                            </div>
+                            <div className="stat">
+                                <h4>Tipo de Usuario:</h4>
+                                <span>{userTypeID || ''}</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        );
+    }
 }
+
+export default AdminInfo;

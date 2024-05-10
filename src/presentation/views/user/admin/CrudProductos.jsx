55861fc0de6ee9b74ba8c4f4aa021412de478958
DarioGB1 <darioguzmanbz@gmail.com>
2024-05-10 02:01:53 -0400

    todo implementado

--- src/presentation/views/user/admin/CrudProductos.jsx
+++ src/presentation/views/user/admin/CrudProductos.jsx
@@ -37,10 +37,11 @@ export default function CrudProductos({ productos }) {
                     </button>
                 </Link>
                 <Link to="/admin/crud-products-excel">
+                    <button>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
                     </svg>
-                    <button>Añadir productos (Excel)</button>
+                    Añadir productos (Excel)</button>
                 </Link>
             </div>
             <div className="table-container">

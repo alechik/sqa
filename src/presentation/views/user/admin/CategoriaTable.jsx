da1dedeea3ce5108bc44a6c4455aa25e76c283ab
DarioGB1 <darioguzmanbz@gmail.com>
2024-06-21 02:51:05 -0400

    confirmacion borrar categoria

--- src/presentation/views/user/admin/CategoriaTable.jsx
+++ src/presentation/views/user/admin/CategoriaTable.jsx
@@ -4,6 +4,8 @@ import './categoriatable.css'
 import { Link } from "react-router-dom";
 import { deleteProduct } from "../../../../infraestructure/api/product.js";
 import { useNavigate } from 'react-router-dom';
+import {deleteProductCategory} from "../../../../infraestructure/api/product_category.js";
+import {confirmAlert} from "react-confirm-alert";
 
 export default function CategoriaTable({ productos , categorys}) {
     const [productList, setProductList] = useState(productos);
@@ -20,7 +22,7 @@ export default function CategoriaTable({ productos , categorys}) {
 
     const handleDeleteProduct = async (categoryId) => {
         try {
-            await deleteProduct(categoryId);
+            await deleteProductCategory(categoryId);
             // Actualizar la lista de productos eliminando el producto con el ID dado
             setCategoryList(categorysList.filter(category => category.id !== categoryId));
         } catch (error) {
@@ -32,6 +34,23 @@ export default function CategoriaTable({ productos , categorys}) {
     const handleEditProduct = (categoryId) => {
         // Navega a la ruta de edición con el ID del producto
         navigate(`/admin/edit-category/${categoryId}`);
+    };
+
+    const confirmDelete = (categoryId, categoryname ) => {
+        confirmAlert({
+            title: 'Confirmar eliminación',
+            message: `¿Estás seguro de que deseas eliminar el usuario "${categoryname}"?`,
+            buttons: [
+                {
+                    label: 'Sí',
+                    onClick: () => handleDeleteProduct(categoryId)
+                },
+                {
+                    label: 'No',
+                    onClick: () => {}
+                }
+            ]
+        });
     };
 
     return (
@@ -72,7 +91,7 @@ export default function CategoriaTable({ productos , categorys}) {
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                                         </svg>
                                     </button>
-                                    <button id='borrar' onClick={() => handleDeleteProduct(categoria.id)}>
+                                    <button id='borrar' onClick={() => confirmDelete(categoria.id,categoria.name)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                         </svg>
